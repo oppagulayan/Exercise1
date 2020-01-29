c16b33885ad2b5bd61d5ec15412e73c0cab93e6b
oppagulayan <oppagulayan1@up.edu.ph>
2020-01-29 07:37:23 +0000

    update on matrix program

--- matrixprogram.c
+++ matrixprogram.c
@@ -2,9 +2,9 @@
 #include<stdlib.h>
 int main()
 {
-FILE *m1 = fopen("matrix1", "m1")
-FILE *m2 = fopen("matrix2", "m2")
-if (m1 = NULL || m2 = NULL)
+FILE *m1 = fopen("matrix1", "m1");
+FILE *m2 = fopen("matrix2", "m2");
+if (m1 == NULL || m2 ==  NULL)
 	{
 	printf("error! Could not open file\n");
 	exit(-1);
