23fb8ee32783ad5b99aacd7e0d1d487ec2708ed3
oppagulayan <oppagulayan1@up.edu.ph>
2020-01-29 07:07:15 +0000

    file input test

--- matrixprogram.c
+++ matrixprogram.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-#placeholder 
+FILE *m1 = fopen("matrix1", "m1")
+FILE *m2 = fopen("matrix2", "m2")
+if (m1 = NULL || m2 = NULL)
+	{
+	printf("error! Could not open file\n");
+	exit(-1);
+	}
 }
